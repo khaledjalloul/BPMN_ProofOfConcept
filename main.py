d0642fb437edf54374835d51df92e9432fe77017
Khaled Jalloul <khaled.jalloul@hotmail.com>
2023-04-17 00:48:00 +0300

    WIP: Search for alternative processes

--- main.py
+++ main.py
@@ -1,34 +1,61 @@
-import time
+import json
 from pybpmn.bpmn_process import BpmnProcess
 from pprint import pprint
 from copy import copy
 from interface import displayInterface
 
-constraints = {
-    "CheckProvisions_enter": {
-        "date": [20230715, 20230730]
-    },
-    "LiquidProteinsFeeding_enter": {
-        "cost": [110, 120]
-    },
-    "LiquidProteinsFeeding_exit": {
-        "weight": [35, 50]
-    }
-}
-
-def checkConstraints(name, position, kargs):
-    token_state = kargs['payload']['token_state']
-    if f"{name}_{position}" in constraints:
-        task_constraints = constraints[f"{name}_{position}"]
-        for constraint in task_constraints:
-            if not (token_state[constraint] >= task_constraints[constraint][0] and token_state[constraint] <= task_constraints[constraint][1]):
-                handleDeviation(kargs=kargs, variable=constraint, value=token_state[constraint], expected_range=f"{task_constraints[constraint][0]} - {task_constraints[constraint][1]}", correct_value = int((task_constraints[constraint][1] + task_constraints[constraint][0]) / 2), stage=position)
+with open('data/constraints.json') as f:
+    constraints = json.load(f)
+    
+with open('data/alternatives.json') as f:
+    alternatives = json.load(f)
     
 def handleDeviation(kargs, variable, value, expected_range, correct_value, stage):
     kargs['payload']['token_state'][variable] = correct_value
     kargs['task'].update({f"error_{stage}": {variable: value}}) # Save wrong value in error
     kargs['task'].update({f'{stage}_state': copy(kargs['payload']['token_state'])}) # Save correct value in payload
     
+def checkConstraints(name, position, kargs):
+    token_state = kargs['payload']['token_state']
+    if position in constraints[name]:
+        task_constraints = constraints[name][position]
+        for constraint in task_constraints:
+            if not (token_state[constraint] >= task_constraints[constraint][0] and token_state[constraint] <= task_constraints[constraint][1]):
+                if position == "exit":
+                    searchForAlternatives(kargs, constraints[name]["index"])
+    
+def searchForAlternatives(kargs, index):
+    token_state = kargs['payload']['token_state']
+    
+    filteredProcesses1 = []
+    for alternative in alternatives:
+        enter_constraints = alternatives[alternative]["enter"]
+        for constraint in enter_constraints:
+            if constraint in token_state:
+                if token_state[constraint] >= enter_constraints[constraint][0] and token_state[constraint] <= enter_constraints[constraint][1]:
+                    filteredProcesses1.append(alternative)
+    print("Possible Solutions (1): ", filteredProcesses1)
+    
+    filteredProcesses2 = []
+    for alternative in filteredProcesses1:
+        exit_values = alternatives[alternative]["exit"]
+        remaining_processes = dict(filter(lambda x: x[1]["index"] > index, constraints.items()))
+        remaining_processes = dict(filter(lambda x: "enter" in x[1], remaining_processes.items()))
+        for process in remaining_processes:
+            isValidExit = True
+            enter_constraints = remaining_processes[process]["enter"]
+            for value in exit_values:
+                if not value in enter_constraints or exit_values[value] > enter_constraints[value][1] or exit_values[value] < enter_constraints[value][0]:
+                    isValidExit = False
+                    break         
+            if (isValidExit):
+                filteredProcesses2.append({
+                    "solution": alternative,
+                    "nextProcess": process
+                })
+    print("Possible Solutions (2): ", filteredProcesses2)
+
+                    
 class Handler():        
     def on_enter_task(self, **kargs):
         if 'token_state' in kargs['payload']:
@@ -54,7 +81,6 @@ class Handler():
         kargs['payload']['token_state']['cost'] += 40
 
     def on_LiquidProteinsFeeding(self, **kargs):
-        checkConstraints("LiquidProteinsFeeding", "enter", kargs)
         while kargs['payload']['token_state']['weight'] < 25:
             kargs['payload']['token_state']['weight'] += 1
             kargs['payload']['token_state']['cost'] += 2
@@ -62,26 +88,23 @@ class Handler():
         checkConstraints("LiquidProteinsFeeding", "exit", kargs)
         
     def on_VarioseVaccineNovember(self, **kargs):
+        checkConstraints("VarioseVaccineNovember", "enter", kargs)
         kargs['payload']['token_state']['weight'] -= 5
         kargs['payload']['token_state']['cost'] += 100
         kargs['payload']['token_state']['date'] += 100
         
 def test_process():
     instance = BpmnProcess()
-    try:
-        instance.start_process(open("simple_bee.xml","r").read(),Handler())
-        state, errors = instance.payload['token_state'], instance.payload['errors']
-        if len(errors) == 0:
-            print("\nProcess Executed. Final State:\n")
-            pprint(instance.payload['token_state'])
-        else:
-            print("\nProcess encountered errors:\n")
-            pprint(errors)
-            pprint(instance.payload)
-        displayInterface(instance.payload)
-    except Exception as e:
-        print(e)
+    instance.start_process(open("models/simple_bee.xml","r").read(),Handler())
+    state, errors = instance.payload['token_state'], instance.payload['errors']
+    if len(errors) == 0:
+        print("\nProcess Executed. Final State:\n")
+        pprint(instance.payload['token_state'])
+    else:
+        print("\nProcess encountered errors:\n")
+        pprint(errors)
+        pprint(instance.payload)
+    # displayInterface(instance.payload)
 
-    
 if __name__ == '__main__':
     test_process()
